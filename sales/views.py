04ed4f507db2e90cfa9a0f0a2a8fe8970d98d7c4
Satyam <stmsng2001@gmail.com>
2022-06-02 12:34:20 +0530

    Cleaned the code for views.py and added a new template for car listings view

--- sales/views.py
+++ sales/views.py
@@ -7,28 +7,14 @@ from sales.models import CarInfo
 
 
 def car_listings(request):
-    return render(request, "sales/car_list.html", {"listings":CarInfo.objects.all()})
+    return render(request, "sales/car_list2.html", {"listings":CarInfo.objects.all()})
 
 
 
 def sell(request):
-    errors = ""
     if request.method == "POST":
         form = SellCarForm(request.POST,request.FILES or None)
         if form.is_valid():
             form.save(commit=False, user = request.user)
             return redirect("sales:home")
-        else:
-            errors_dict = form.errors
-            # breakpoint()
-            for i,error in errors_dict.items():
-                errors += '"' + str(i) + '"'
-                errors = errors + " " +error[0]
-
-            print("not valid")
-            # breakpoint()
-    form = SellCarForm()    
-    context = {"form":form}
-    if errors != "":
-        context["errors"] = errors
-    return render(request, "sales/sell.html", context)
+    return render(request, "sales/sell.html", {"form":SellCarForm()})
